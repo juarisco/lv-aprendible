b378eee60d18811dbc41a25cef625e0a9a58718a
juarisco <juarisco@outlook.com>
2019-07-19 11:53:12 -0500

    03. Route named

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('saludo/{nombre?}', function ($nombre = "Invitado") {
-    return "Saludos " . $nombre;
+Route::get('saludo/{nombre?}', function ($nombre = 'Invitado') {
+    return 'Saludos ' . $nombre;
+});
+
+Route::get('contactame', function () {
+    return 'Sección de contactos';
+})->name('contactos');
+
+Route::get('/', function () {
+    echo "<a href='" . route('contactos') . "'>Contactos 1</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 2</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 3</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 4</a><br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 5</a><br>";
 });
