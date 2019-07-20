b27b3a6037a65f51e1da833f6c04aedc21c44015
juarisco <juarisco@outlook.com>
2019-07-20 12:32:51 -0500

    09.-Enviar-formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
     
-    <h1>Este es el contact</h1>
+    <h1>Contact</h1>
+
+    <form action="{{ route('contact') }}" method="POST">
+        @csrf
+        <input type="text" name="name" placeholder="Nombre..." value="Josef"><br>
+        <input type="email" name="email" placeholder="Email..." value="josef@mail.com"><br>
+        <input type="text" name="subject" placeholder="Asunto..." value="Asund de Josef"><br>
+        <textarea name="content" placeholder="Mensaje...">Mensaje de prueba</textarea><br>
+        <button type="submit">Enviar</button>
+    </form>
     
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::view('/about', 'about')->name('about');
 Route::get('/portafolio', 'PortafolioController@index')->name('portafolio');
 Route::view('/contact', 'contact')->name('contact');
 
+Route::post('contact','MessagesController@store');
+
 // Route::resource('projects', 'PortafolioController');
