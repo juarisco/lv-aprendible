a44af33764eb366ec0897ece6de8e4fcfc9b0380
juarisco <juarisco@outlook.com>
2019-08-01 17:15:33 -0500

    28. Introducción a Laravel Mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','Aprendible')</title>
-    <style>
-        .active a{
-            color: red;
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
 
