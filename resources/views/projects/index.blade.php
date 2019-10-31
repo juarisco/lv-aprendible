a6438fa4b4cd59c405c5e6148cd811f2d9172a32
Josef <juarisco@outlook.com>
2019-10-31 15:19:02 -0500

    35.-Cómo-mostrar-imágenes

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -15,19 +15,36 @@
     
     <p class="lead text-secondary">Proyectos realizados Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
     
-    <ul class="list-group">
+    <div class="d-flex flex-wrap justify-content-between align-items-start">
         @forelse ($projects as $project)
-        <li class="list-group-item border-0 mb-3 shadow-sm">
-            <a class="text-secondary d-flex justify-content-between align-items-center" href="{{ route('projects.show', $project) }}">
-                <span class="font-weight-bold">{{ $project->title }}</span>
-                <span class="text-black-50">{{ $project->created_at->format('d/m/Y') }}</span>
-            </a>
-        </li>
+            <div class="card border-0 shadow-sm mt-4 mx-auto" style="width: 18rem">
+                @if ($project->image)
+                    <img class="card-img-top" style="height: 150px; object-fit: cover"
+                        src="/storage/{{ $project->image }}" 
+                        alt="{{ $project->title }}">
+                @endif
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a href="{{ route('projects.show', $project) }}">{{ $project->title }}</a>
+                    </h5>
+                    <h6 class="card-subtitle">{{ $project->created_at->format('d/m/Y') }}</h6>
+                    <p class="card-text text-truncate">{{ $project->description }}</p>
+                    <a href="{{ route('projects.show', $project) }}" 
+                        class="btn btn-primary btn-sm">
+                    Ver más..</a>
+                </div>
+            </div>
         @empty
-        <li class="list-group-item border-0 mb-3 shadow-sm">@lang('There\'re not projects to show')</li>
-        @endforelse 
-        {{ $projects->links() }}
-    </ul>
+            <div class="card">
+                <div class="card-body">
+                    @lang('There\'re not projects to show')
+                </div>
+            </div>
+        @endforelse
+        <div class="mt-4">
+            {{ $projects->links() }}
+        </div>
+    </div>
     
 </div>
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,27 +4,36 @@
 
 @section('content')
 <div class="container">
-    <div class="bg-white p-5 shadow rounded">
-        <h1>{{ $project->title }}</h1>
-        <p class="text-secondary">{{ $project->description }}</p>
-        <p class="text-black-50">{{ $project->created_at->diffForHumans() }}</p>
-        <div class="d-flex justify-content-between align-items-center">
-            <a href="{{ route('projects.index') }}">Regresar</a>
-            @auth
-            <div class="btn-group btn-group-sm">
-    
-                <a class="btn btn-primary" href="{{ route('projects.edit',$project) }}">Editar</a>
-                <a class="btn btn-danger" href="#" onclick="document.getElementById('delete-project').submit()">@lang('Delete')</a>
-                
-                <form id="delete-project"
-                    class="d-none"
-                    action="{{ route('projects.destroy', $project) }}" method="post">
-                    @csrf @method('DELETE')
-                </form>
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-8 mx-auto">
+            @if ($project->image)
+                <img class="card-img-top"
+                    src="/storage/{{ $project->image }}" 
+                    alt="{{ $project->title }}">
+            @endif
+            <div class="bg-white p-5 shadow rounded">
+                <h1>{{ $project->title }}</h1>
+                <p class="text-secondary">{{ $project->description }}</p>
+                <p class="text-black-50">{{ $project->created_at->diffForHumans() }}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="{{ route('projects.index') }}">Regresar</a>
+                    @auth
+                        <div class="btn-group btn-group-sm">
+
+                            <a class="btn btn-primary" href="{{ route('projects.edit',$project) }}">@lang('Edit')</a>
+                            <a class="btn btn-danger" href="#" onclick="document.getElementById('delete-project').submit()">@lang('Delete')</a>
+                            
+                            <form id="delete-project"
+                                class="d-none"
+                                action="{{ route('projects.destroy', $project) }}" method="post">
+                                @csrf @method('DELETE')
+                            </form>
+                        </div>
+                    @endauth
+                </div>
+                    
             </div>
-            @endauth
         </div>
-            
     </div>
 </div>
 @endsection
